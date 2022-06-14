539fcbba7db717989ca7c932a81d4fcec75433fc
WFRISCH <wilhelm.frisch@freenet.de>
2022-06-14 13:10:06 +0200

    Made the header behave nicely with responsive and scroll behaviour

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/* When the user scrolls down, hide the header. When the user scrolls up, show the header */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-88px";
+    document.getElementById("navbarToggler").classList.remove("collapse");
+    document.getElementById("navbarToggler").classList.remove("show");
+    document.getElementById("navbarToggler").classList.add("collapsing");
+  }
+  prevScrollpos = currentScrollPos;
+} 

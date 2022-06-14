f195cd9fa9f18c67197b8f0a8cc7ad01bfe1c84c
WFRISCH <wilhelm.frisch@freenet.de>
2022-06-14 13:32:57 +0200

    Fixed bug when scrolling up on large screens, that the nav links were not showing up again

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("header").style.top = "0";
+    document.getElementById("navbarToggler").classList.remove("collapsing");
+    document.getElementById("navbarToggler").classList.add("collapse");
   } else {
     document.getElementById("header").style.top = "-88px";
     document.getElementById("navbarToggler").classList.remove("collapse");
